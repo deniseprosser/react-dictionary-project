f2ffc161f17105594949d8d199293a54d9086ddb
deniseprosser <denise.prosser23@gmail.com>
2024-10-11 09:32:06 +0100

    added API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyword}`);
+    let apiKey = "aab3d3obc0205ebt561454f09dff85ec";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeyword(event) {
