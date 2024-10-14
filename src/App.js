c85f1f9cb752eb3bdc1ffa5183936a1308a682b1
deniseprosser <denise.prosser23@gmail.com>
2024-10-14 15:43:58 +0100

    Added footer with links

--- src/App.js
+++ src/App.js
@@ -7,6 +7,32 @@ export default function App() {
       <div className="container">
         <Dictionary />
       </div>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/deniseprosser"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Denise Prosser
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/deniseprosser/react-dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://zesty-kashata-fa5ed8.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
