b88e5f9b37ebb8d1ae483439d60f5c50c912c39e
deniseprosser <denise.prosser23@gmail.com>
2024-10-21 15:57:56 +0100

    Added synonym and example components

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Synonyms from "./Synonyms";
+import Example from "./Example";
 
 export default function Meaning(props) {
   return (
@@ -6,7 +8,9 @@ export default function Meaning(props) {
       <h3>{props.meaning.partOfSpeech}</h3>
       <div>
         <p>{props.meaning.definition}</p>
-        <em>{props.meaning.example}</em>
+        <Example example={props.meaning.example} />
+
+        <Synonyms synonyms={props.meaning.synonyms} />
       </div>
     </div>
   );
